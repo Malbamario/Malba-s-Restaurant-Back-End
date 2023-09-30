b823dcc41c28db97244c3a912fd23f8e0e905a16
Malbamario <malba324@gmail.com>
2023-09-30 08:04:05 +0800

    update with the right wy to enable cors

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const init = async () => {
     const server = hapi.server({
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         port: 5000,
+        "routes": {
+            "cors": true
+        }
     });
 
     server.route(routes);
-    server.connection({ routes: { cors: true } }) 
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
